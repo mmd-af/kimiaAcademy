9e943de280a81104d6cd71c8bb9894cfde3fec84
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-05-09 11:46:01 +0430

    navbar and search box is ok

---
+++ resources/views/site/layouts/components/search.blade.php
@@ -0,0 +1,22 @@
+<!-- Modal -->
+<div class="modal fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+
+
+            </div>
+            <div class="modal-body">
+                <input class="form-control" type="text" name="search" placeholder="کاکو دنبال چی میگردی...؟">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">جستجو</button>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/site/layouts/partials/navbar.blade.php
+++ resources/views/site/layouts/partials/navbar.blade.php
@@ -1,46 +1,33 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">گروه آموزشی دارویی کیمیاگر</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">خانه<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">دوره های آموزشی</a>
-            </li>
+<nav class="navbar navbar-expand-lg navbar-dark bg-mine py-3">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">گروه آموزشی دارویی کیمیاگر</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto ">
+                <li class="nav-item active">
+                    <a class="nav-link mr-4" href="#">خانه <span class="sr-only">(current)</span></a>
+                </li>
 
-{{--            <li class="nav-item dropdown">--}}
-{{--                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"--}}
-{{--                   aria-haspopup="true" aria-expanded="false">--}}
-{{--                    Dropdown--}}
-{{--                </a>--}}
-{{--                <div class="dropdown-menu" aria-labelledby="navbarDropdown">--}}
-{{--                    <a class="dropdown-item" href="#">Action</a>--}}
-{{--                    <a class="dropdown-item" href="#">Another action</a>--}}
-{{--                    <div class="dropdown-divider"></div>--}}
-{{--                    <a class="dropdown-item" href="#">Something else here</a>--}}
-{{--                </div>--}}
-{{--            </li>--}}
+                <li class="nav-item">
+                    <a class="nav-link mr-4" href="#">محصولات</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mr-4" href="#">مقالات آموزشی</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mr-4" href="#">تماس با ما</a>
+                </li>
 
-            <li class="nav-item">
-                <a class="nav-link" href="#">محصولات</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">مقالات آموزشی</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">تماس با ما</a>
-            </li>
+            </ul>
+            <a class="text-light ml-4" data-toggle="modal" data-target="#searchModal"><i class="fa fa-search fa-lg"
+                                                                                         aria-hidden="true"></i></a>
+            <a class="btn btn-outline-light" href="{{ url('/admin/dashboard') }}"
+               class="text-sm text-gray-700 dark:text-gray-500 underline">ناحیه کاربری</a>
 
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">جستجو</button>
-        </form>
+        </div>
     </div>
 </nav>
-سلام بر دوستان عزیز و ارجمند
+@include('site.layouts.components.search')
