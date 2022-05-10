6adece4bf9bd119e221e600b6b4b95596e855b59
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-05-10 14:56:52 +0430

    add repositories

---
+++ app/Repositories/Admin/BaseAdminRepository.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Repositories\Admin;
+
+abstract class BaseAdminRepository extends Repository
+{
+}
---
+++ app/Repositories/Admin/CourseRepository.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Repositories\Admin;
+
+class PostRepository extends BaseAdminRepository
+{
+}
