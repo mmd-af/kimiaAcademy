83d476f8c1014a58182c29526f6246434bf5ec64
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-05-10 13:39:40 +0430

    add course Request

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -38,7 +38,7 @@ class CourseController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CourseRequest $request)
     {
         dd($request->all());
     }
