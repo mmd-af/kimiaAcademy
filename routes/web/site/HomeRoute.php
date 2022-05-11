e47cfc64bf23b56d3ff8323bca976b7a4796a289
Alimohammad-Hoodi <62663076+Alimohammad-Hoodi@users.noreply.github.com>
2022-05-11 13:25:29 +0430

    Update HomeController route

--- routes/web/site/HomeRoute.php
+++ routes/web/site/HomeRoute.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['web'], 'namespace' => 'App\Http\Controllers\Home'], function () {
+Route::group(['middleware' => ['web'], 'namespace' => 'App\Http\Controllers\Site'], function () {
     Route::get('/', [
         'as' => 'home',
-        'uses' => 'HomeController@home'
+        'uses' => 'Home\HomeController@index'
     ]);
 });
