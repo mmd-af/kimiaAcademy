23ce670dc6c250dc42ccc9bea1be46e2a826e007
Alimohammad-Hoodi <62663076+Alimohammad-Hoodi@users.noreply.github.com>
2022-05-11 13:24:06 +0430

    Update app.blade.php

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -2,48 +2,6 @@
 
 @include('site.layouts.partials.header')
 
-
 @yield('content')
-
-<div class="container mb-5 mt-5">
-    <div class="row justify-content-md-center">
-
-        <div class="col-md-4 ">
-            <div class="card mt-3 shadow">
-                <div class="product-1 align-items-center p-2 text-center">
-                    <div class="image-container">
-                        <img class="d-block w-100" src="holder.js/400x300?auto=yes&bg=666&fg=444&text=picture"
-                             alt="Second slide"></div>
-                    <div class="product-description py-2">
-                        <h5>عنوان اصلی</h5>
-                        <div class="mt-3 info"><span class="text1 d-block">توضیحات کوتاه</span></div>
-                        <div class=" cost mt-3 text-dark"><span>14000 تومان</span>
-                        </div>
-                    </div>
-                </div>
-                <button type="button" class="btn text-light btn-product btn-lg btn-block">خرید</button>
-            </div>
-        </div>
-
-
-        <div class="col-md-4 ">
-            <div class="card mt-3 shadow">
-                <div class="product-1 align-items-center p-2 text-center">
-                    <div class="image-container">
-                        <img class="d-block w-100" src="holder.js/400x300?auto=yes&bg=666&fg=444&text=picture"
-                             alt="Second slide"></div>
-                    <div class="product-description py-2">
-                        <h5>عنوان اصلی</h5>
-                        <div class="mt-3 info"><span class="text1 d-block">توضیحات کوتاه</span></div>
-                        <div class=" cost mt-3 text-dark"><span>14000 تومان</span>
-                        </div>
-                    </div>
-                </div>
-                <button type="button" class="btn text-light btn-product btn-lg btn-block">خرید</button>
-            </div>
-        </div>
-
-    </div>
-</div>
 
 @include('site.layouts.partials.footer')
