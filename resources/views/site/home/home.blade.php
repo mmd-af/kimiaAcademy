9b0f5895937864cc175ce24abb320a7c59d9f6db
Alimohammad-Hoodi <62663076+Alimohammad-Hoodi@users.noreply.github.com>
2022-05-13 16:52:51 +0430

    Add home-hr-text in app.scss

--- resources/views/site/home/home.blade.php
+++ resources/views/site/home/home.blade.php
@@ -136,7 +136,6 @@
 
         </div>
 
-
     </div>
 
     <div class="course-education half-circle ">
@@ -163,11 +162,13 @@
                                     <h5 class="text-center">عنوان اصلی</h5>
 
 
-                                    <div class="mt-3 info px-2"><a href="" class="text-gray"><img class="aparat-icon mx-2"
-                                                                src="{{asset('assets/site/images/svg/aparat-25x25.svg')}}"
-                                                                alt="">
+                                    <div class="mt-3 info px-2"><a href="" class="text-gray"><img
+                                                class="aparat-icon mx-2"
+                                                src="{{asset('assets/site/images/svg/aparat-25x25.svg')}}"
+                                                alt="">
                                             لینک آپارات</a></div>
-                                    <div class="mt-3 info px-2"><a href="" ><i class="fab fa-youtube mx-2 aparat-icon fa-lg"></i> لینک یوتیوب</a></div>
+                                    <div class="mt-3 info px-2"><a href=""><i
+                                                class="fab fa-youtube mx-2 aparat-icon fa-lg"></i> لینک یوتیوب</a></div>
 
                                 </div>
 
@@ -187,11 +188,13 @@
                                     <h5 class="text-center">عنوان اصلی</h5>
 
 
-                                    <div class="mt-3 info px-2"><a href="" class="text-gray"><img class="aparat-icon mx-2"
-                                                                                                  src="{{asset('assets/site/images/svg/aparat-25x25.svg')}}"
-                                                                                                  alt="">
+                                    <div class="mt-3 info px-2"><a href="" class="text-gray"><img
+                                                class="aparat-icon mx-2"
+                                                src="{{asset('assets/site/images/svg/aparat-25x25.svg')}}"
+                                                alt="">
                                             لینک آپارات</a></div>
-                                    <div class="mt-3 info px-2"><a href="" ><i class="fab fa-youtube mx-2 aparat-icon fa-lg"></i> لینک یوتیوب</a></div>
+                                    <div class="mt-3 info px-2"><a href=""><i
+                                                class="fab fa-youtube mx-2 aparat-icon fa-lg"></i> لینک یوتیوب</a></div>
 
                                 </div>
 
@@ -211,11 +214,13 @@
                                     <h5 class="text-center">عنوان اصلی</h5>
 
 
-                                    <div class="mt-3 info px-2"><a href="" class="text-gray"><img class="aparat-icon mx-2"
-                                                                                                  src="{{asset('assets/site/images/svg/aparat-25x25.svg')}}"
-                                                                                                  alt="">
+                                    <div class="mt-3 info px-2"><a href="" class="text-gray"><img
+                                                class="aparat-icon mx-2"
+                                                src="{{asset('assets/site/images/svg/aparat-25x25.svg')}}"
+                                                alt="">
                                             لینک آپارات</a></div>
-                                    <div class="mt-3 info px-2"><a href="" ><i class="fab fa-youtube mx-2 aparat-icon fa-lg"></i> لینک یوتیوب</a></div>
+                                    <div class="mt-3 info px-2"><a href=""><i
+                                                class="fab fa-youtube mx-2 aparat-icon fa-lg"></i> لینک یوتیوب</a></div>
 
                                 </div>
 
@@ -235,11 +240,13 @@
                                     <h5 class="text-center">عنوان اصلی</h5>
 
 
-                                    <div class="mt-3 info px-2"><a href="" class="text-gray"><img class="aparat-icon mx-2"
-                                                                                                  src="{{asset('assets/site/images/svg/aparat-25x25.svg')}}"
-                                                                                                  alt="">
+                                    <div class="mt-3 info px-2"><a href="" class="text-gray"><img
+                                                class="aparat-icon mx-2"
+                                                src="{{asset('assets/site/images/svg/aparat-25x25.svg')}}"
+                                                alt="">
                                             لینک آپارات</a></div>
-                                    <div class="mt-3 info px-2"><a href="" ><i class="fab fa-youtube mx-2 aparat-icon fa-lg"></i> لینک یوتیوب</a></div>
+                                    <div class="mt-3 info px-2"><a href=""><i
+                                                class="fab fa-youtube mx-2 aparat-icon fa-lg"></i> لینک یوتیوب</a></div>
 
                                 </div>
 
@@ -256,9 +263,143 @@
             </div>
         </div>
 
+    </div>
+    <div class="container mb-5 mt-5 ">
+        {{--TODO admin dashbord--}}
+        <div class="text-center mb-5">
+            <h3 class="home-hr-text">مقالات آموزشی داروشناسی</h3>
+        </div>
+
+        <div class="row  justify-content-center ">
+
+            <div class=" slick-carousel">
+                <div class="col-md-12 col-sm-6">
+                    <div class="card mt-3 shadow ">
+
+                        <div class="card-body product-1 align-items-center p-2 text-center">
+                            <div class="image-container">
+                                <img class="d-block w-100" src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture"
+                                     alt="Second slide"></div>
+                            <div class="product-description py-2">
+                                <h5>عنوان اصلی</h5>
+                                <div class="mt-3 info"><span class="text1 d-block">توضیحات کوتاه</span></div>
+                                <div class=" cost mt-3 text-dark"><span>14000 تومان</span>
+                                </div>
+                            </div>
+
+                        </div>
+                        {{--TODO btn hover--}}
+                        <a type="button" class="btn text-light btn-product btn-lg btn-block stretched-link"
+                           href="/home">خرید</a>
+                    </div>
+                </div>
+                <div class="col-md-12 col-sm-6 py-4">
+                    <div class="card mt-3 shadow ">
+
+                        <div class="card-body product-1 align-items-center p-2 text-center">
+                            <div class="image-container">
+                                <img class="d-block w-100" src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture"
+                                     alt="Second slide"></div>
+                            <div class="product-description py-2">
+                                <h5>عنوان اصلی</h5>
+                                <div class="mt-3 info"><span class="text1 d-block">توضیحات کوتاه</span></div>
+                                <div class=" cost mt-3 text-dark"><span>14000 تومان</span>
+                                </div>
+                            </div>
+
+                        </div>
+                        {{--TODO btn hover--}}
+                        <a type="button" class="btn text-light btn-product btn-lg btn-block stretched-link"
+                           href="/home">خرید</a>
+                    </div>
+                </div>
+                <div class="col-md-12 col-sm-6 py-4">
+                    <div class="card mt-3 shadow ">
+
+                        <div class="card-body product-1 align-items-center p-2 text-center">
+                            <div class="image-container">
+                                <img class="d-block w-100" src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture"
+                                     alt="Second slide"></div>
+                            <div class="product-description py-2">
+                                <h5>عنوان اصلی</h5>
+                                <div class="mt-3 info"><span class="text1 d-block">توضیحات کوتاه</span></div>
+                                <div class=" cost mt-3 text-dark"><span>14000 تومان</span>
+                                </div>
+                            </div>
+
+                        </div>
+                        {{--TODO btn hover--}}
+                        <a type="button" class="btn text-light btn-product btn-lg btn-block stretched-link"
+                           href="/home">خرید</a>
+                    </div>
+                </div>
+                <div class="col-md-12 col-sm-6 py-4">
+                    <div class="card mt-3 shadow ">
+
+                        <div class="card-body product-1 align-items-center p-2 text-center">
+                            <div class="image-container">
+                                <img class="d-block w-100" src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture"
+                                     alt="Second slide"></div>
+                            <div class="product-description py-2">
+                                <h5>عنوان اصلی</h5>
+                                <div class="mt-3 info"><span class="text1 d-block">توضیحات کوتاه</span></div>
+                                <div class=" cost mt-3 text-dark"><span>14000 تومان</span>
+                                </div>
+                            </div>
+
+                        </div>
+                        {{--TODO btn hover--}}
+                        <a type="button" class="btn text-light btn-product btn-lg btn-block stretched-link"
+                           href="/home">خرید</a>
+                    </div>
+                </div>
+                <div class="col-md-12 col-sm-6 py-4">
+                    <div class="card mt-3 shadow ">
+
+                        <div class="card-body product-1 align-items-center p-2 text-center">
+                            <div class="image-container">
+                                <img class="d-block w-100" src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture"
+                                     alt="Second slide"></div>
+                            <div class="product-description py-2">
+                                <h5>عنوان اصلی</h5>
+                                <div class="mt-3 info"><span class="text1 d-block">توضیحات کوتاه</span></div>
+                                <div class=" cost mt-3 text-dark"><span>14000 تومان</span>
+                                </div>
+                            </div>
+
+                        </div>
+                        {{--TODO btn hover--}}
+                        <a type="button" class="btn text-light btn-product btn-lg btn-block stretched-link"
+                           href="/home">خرید</a>
+                    </div>
+                </div>
+                <div class="col-md-12 col-sm-6 py-4">
+                    <div class="card mt-3 shadow ">
+
+                        <div class="card-body product-1 align-items-center p-2 text-center">
+                            <div class="image-container">
+                                <img class="d-block w-100" src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture"
+                                     alt="Second slide"></div>
+                            <div class="product-description py-2">
+                                <h5>عنوان اصلی</h5>
+                                <div class="mt-3 info"><span class="text1 d-block">توضیحات کوتاه</span></div>
+                                <div class=" cost mt-3 text-dark"><span>14000 تومان</span>
+                                </div>
+                            </div>
+
+                        </div>
+                        {{--TODO btn hover--}}
+                        <a type="button" class="btn text-light btn-product btn-lg btn-block stretched-link"
+                           href="/home">خرید</a>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
 
     </div>
-
 
 @endsection
 
