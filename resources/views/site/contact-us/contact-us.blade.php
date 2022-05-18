68c175493e5cfe13765872fc85486762fb459218
mojtaba mohammadi <mojtaba.m2021@gmail.com>
2022-05-18 16:22:49 +0430

    Frontend : add page contact-us , blog route

---
+++ resources/views/site/contact-us/contact-us.blade.php
@@ -0,0 +1,19 @@
+@extends('site.layouts.app')
+@section('title','کیمیا آکادمی')
+@section('content')
+    <div class="container">
+        <div class="col-md-4 shadow-sm">
+
+        </div>
+        <div class="col-md-8 ">
+            <div class="row">
+
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
+
+
