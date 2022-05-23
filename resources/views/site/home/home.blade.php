dd557146699b6fede66cd554ed8c35484cfb72f1
Alimohammad-Hoodi <62663076+Alimohammad-Hoodi@users.noreply.github.com>
2022-05-23 09:59:54 +0430

    Edit card in home

--- resources/views/site/home/home.blade.php
+++ resources/views/site/home/home.blade.php
@@ -12,21 +12,18 @@
         <div class="row  justify-content-center ">
 
             <div class=" slick-carousel">
-                <div class="col-md-12 col-sm-6">
+                <div class="col-md-12 col-sm-6 py-4">
                     <div class="card mt-3 shadow ">
-
                         <div class="card-body product-1 align-items-center p-2 text-center">
                             <div class="image-container">
                                 <img class="d-block w-100" src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture"
-                                     alt="Second slide">
-                            </div>
+                                     alt="Second slide"></div>
                             <div class="product-description py-2">
                                 <h5>عنوان اصلی</h5>
                                 <div class="mt-3 info"><span class="text1 d-block">توضیحات کوتاه</span></div>
                                 <div class=" cost mt-3 text-dark"><span>14000 تومان</span>
                                 </div>
                             </div>
-
                         </div>
                         {{--TODO btn hover--}}
                         <a type="button" class="btn text-light btn-product btn-lg btn-block stretched-link"
