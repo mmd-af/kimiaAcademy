6e34263e16240f57e56542c21ff90a64bf459fb5
mojtaba mohammadi <mojtaba.m2021@gmail.com>
2022-05-24 14:05:59 +0430

    blog page done

--- resources/views/site/blog/blog.blade.php
+++ resources/views/site/blog/blog.blade.php
@@ -1,16 +1,224 @@
 @extends('site.layouts.app')
 @section('title','کیمیا آکادمی')
 @section('content')
-    <div class="container">
-        <div class="col-md-4 shadow-sm">
+    <!-- ======= Blog Section ======= -->
+    <section id="blog" class="blog">
+        <div class="container" data-aos="fade-up">
 
-        </div>
-        <div class="col-md-8 ">
             <div class="row">
 
+                <div class="col-lg-8 entries">
+
+                    <article class="entry">
+
+                        <div class="entry-img">
+                            <img src="holder.js/800x400?auto=yes&bg=666&fg=444&text=picture" alt="" class="img-fluid">
+                        </div>
+
+                        <h2 class="entry-title">
+                            <a href="blog-single.html">
+                                عنوان مقاله
+                            </a>
+                        </h2>
+
+                        <div class="entry-meta ">
+                            <ul class="">
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-single.html">نویسنده : رضا معاف پوریان</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01"> تاریخ : 1401/03/01</time></a></li>
+                            </ul>
+                        </div>
+
+                        <div class="entry-content">
+                            <p>
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+                            </p>
+                            <div class="align-self-end pb-3">
+                                <a href="#" class="btn btn-article float-left align-bottom">مطالعه بیشتر</a>
+                            </div>
+                        </div>
+
+                    </article><!-- End blog entry -->
+
+                    <article class="entry">
+
+                        <div class="entry-img">
+                            <img src="holder.js/800x400?auto=yes&bg=666&fg=444&text=picture" alt="" class="img-fluid">
+                        </div>
+
+                        <h2 class="entry-title">
+                            <a href="blog-single.html">
+                                عنوان مقاله
+                            </a>
+                        </h2>
+
+                        <div class="entry-meta ">
+                            <ul class="">
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-single.html">نویسنده : رضا معاف پوریان</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01"> تاریخ : 1401/03/01</time></a></li>
+                            </ul>
+                        </div>
+
+                        <div class="entry-content">
+                            <p>
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+                            </p>
+                            <div class="align-self-end pb-3">
+                                <a href="#" class="btn btn-article float-left align-bottom">مطالعه بیشتر</a>
+                            </div>
+                        </div>
+
+                    </article><!-- End blog entry -->
+
+                    <article class="entry">
+
+                        <div class="entry-img">
+                            <img src="holder.js/800x400?auto=yes&bg=666&fg=444&text=picture" alt="" class="img-fluid">
+                        </div>
+
+                        <h2 class="entry-title">
+                            <a href="blog-single.html">
+                                عنوان مقاله
+                            </a>
+                        </h2>
+
+                        <div class="entry-meta ">
+                            <ul class="">
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-single.html">نویسنده : رضا معاف پوریان</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01"> تاریخ : 1401/03/01</time></a></li>
+                            </ul>
+                        </div>
+
+                        <div class="entry-content">
+                            <p>
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+                            </p>
+                            <div class="align-self-end pb-3">
+                                <a href="#" class="btn btn-article float-left align-bottom">مطالعه بیشتر</a>
+                            </div>
+                        </div>
+
+                    </article><!-- End blog entry -->
+
+                    <article class="entry">
+
+                        <div class="entry-img">
+                            <img src="holder.js/800x400?auto=yes&bg=666&fg=444&text=picture" alt="" class="img-fluid">
+                        </div>
+
+                        <h2 class="entry-title">
+                            <a href="blog-single.html">
+                                عنوان مقاله
+                            </a>
+                        </h2>
+
+                        <div class="entry-meta ">
+                            <ul class="">
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-single.html">نویسنده : رضا معاف پوریان</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01"> تاریخ : 1401/03/01</time></a></li>
+                            </ul>
+                        </div>
+
+                        <div class="entry-content">
+                            <p>
+                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد
+                            </p>
+                            <div class="read-more align-self-end pb-3">
+                                <a href="#" class="btn btn-article float-left align-bottom">مطالعه بیشتر</a>
+                            </div>
+                        </div>
+
+                    </article><!-- End blog entry -->
+                    <div class="blog-pagination">
+                        <ul class="justify-content-center">
+                            <li><a href="#">1</a></li>
+                            <li class="active"><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                        </ul>
+                    </div>
+
+                </div><!-- End blog entries list -->
+
+                <div class="col-lg-4">
+
+                    <div class="sidebar">
+
+                        <h3 class="sidebar-title">جستجو</h3>
+                        <div class="sidebar-item search-form">
+                            <form action="">
+
+                                <input type="text" class="form-control">
+                                <button type="submit" class=""><i class="fa fa-search"></i></button>
+
+                            </form>
+                        </div><!-- End sidebar search formn-->
+
+                        <h3 class="sidebar-title">دسته بندی ها</h3>
+                        <div class="sidebar-item categories">
+                            <ul>
+                                <li><a href="#">دسته اول <span>(25)</span></a></li>
+                                <li><a href="#">دسته دوم <span>(12)</span></a></li>
+                                <li><a href="#">دسته سوم <span>(5)</span></a></li>
+                                <li><a href="#">دسته چهارم <span>(22)</span></a></li>
+                                <li><a href="#">دسته پنجم <span>(8)</span></a></li>
+                            </ul>
+                        </div><!-- End sidebar categories-->
+
+                        <h3 class="sidebar-title">مطالب اخیر</h3>
+                        <div class="sidebar-item recent-posts">
+                            <div class="post-item clearfix">
+                                <img src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture" alt="">
+                                <h4><a href="blog-single.html">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم</a></h4>
+                                <time datetime="2020-01-01">2 روز قبل</time>
+                            </div>
+
+                            <div class="post-item clearfix">
+                                <img src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture" alt="">
+                                <h4><a href="blog-single.html">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم</a></h4>
+                                <time datetime="2020-01-01">3 روز قبل</time>
+                            </div>
+
+                            <div class="post-item clearfix">
+                                <img src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture" alt="">
+                                <h4><a href="blog-single.html">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم</a></h4>
+                                <time datetime="2020-01-01">14 روز قبل</time>
+                            </div>
+
+                            <div class="post-item clearfix">
+                                <img src="holder.js/50x50?auto=yes&bg=666&fg=444&text=picture" alt="">
+                                <h4><a href="blog-single.html">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم</a></h4>
+                                <time datetime="2020-01-01">2 ماه پیش</time>
+                            </div>
+
+
+
+                        </div><!-- End sidebar recent posts-->
+
+{{--                        <h3 class="sidebar-title">Tags</h3>--}}
+{{--                        <div class="sidebar-item tags">--}}
+{{--                            <ul>--}}
+{{--                                <li><a href="#">App</a></li>--}}
+{{--                                <li><a href="#">IT</a></li>--}}
+{{--                                <li><a href="#">Business</a></li>--}}
+{{--                                <li><a href="#">Mac</a></li>--}}
+{{--                                <li><a href="#">Design</a></li>--}}
+{{--                                <li><a href="#">Office</a></li>--}}
+{{--                                <li><a href="#">Creative</a></li>--}}
+{{--                                <li><a href="#">Studio</a></li>--}}
+{{--                                <li><a href="#">Smart</a></li>--}}
+{{--                                <li><a href="#">Tips</a></li>--}}
+{{--                                <li><a href="#">Marketing</a></li>--}}
+{{--                            </ul>--}}
+{{--                        </div><!-- End sidebar tags-->--}}
+
+                    </div><!-- End sidebar -->
+
+                </div><!-- End blog sidebar -->
+
             </div>
+
         </div>
-    </div>
+    </section><!-- End Blog Section -->
+
 
 
 
