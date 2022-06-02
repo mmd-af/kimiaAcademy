3c5f870593d3fb73811029f41fd9be989262bff4
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-06-02 16:35:13 +0430

    add post and user seed

---
+++ database/factories/Post/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories\Post;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => 1,
+            'title' => $this->faker->realTextBetween(5, 15),
+            'slug' => $this->faker->realTextBetween(5, 15),
+            'description' => $this->faker->realTextBetween(500, 1000),
+            'is_active' => 1,
+        ];
+    }
+
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use function now;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -18,7 +19,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+//            'username' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
