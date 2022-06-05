8f0410b578be7bc06a91b516530934675be27fa4
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-06-05 11:37:32 +0430

    Edit category create frontEnd

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -8,59 +8,60 @@
 
     <!-- Content Row -->
     <div class="row">
-        <div class="col-xl-12 col-md-12 mb-4 p-4 bg-white">
-            <div class="mb-4 text-center text-md-right">
-                <h5 class="font-weight-bold">ایجاد دسته بندی</h5>
-            </div>
-            <hr>
-            @include('admin.layouts.partials.errors')
-            <form action="{{ route('admin.categories.store') }}" method="POST">
-                @csrf
-                <div class="form-row">
-                    <div class="form-group col-md-12 mt-3">
-                        <div class="form-group col-md-3">
-                            <label for="Category_title">نام دسته بندی:</label>
-                            <input class="form-control" id="Category_title" name="Category_title" type="text"
-                                   value="{{ old('Category_title') }}">
-                        </div>
-                    </div>
-                    <div class="form-group col-md-12 mt-3">
-                        <div class="form-group col-md-3 mt">
-                            <label for="slug">نام دسته بندی به انگلیسی:</label>
-                            <input class="form-control" id="slug" name="slug" type="text"
-                                   value="{{ old('slug') }}">
-                        </div>
-                    </div>
-
-                    <div class="form-group col-md-12 mt-3">
-                        <label for="type" class="pr-3">انتخاب دسته:</label>
-                        <div class="form-group col-md-3" id="select_category">
-                            <label for="course">دوره های آموزشی</label>
-                            <input id="course" class="category_type" type="radio" value="1" name="cat_type">
-                            <label for="post" class="pr-5">مقالات</label>
-                            <input id="post" class="category_type" type="radio" value="2" name="cat_type">
-                        </div>
-                    </div>
-
-                    <div class="form-group col-md-12 mt-3">
-                        <div class="form-group col-md-3">
-                            <label for="parent_id">نوع دسته</label>
-                            <select class="form-control category-select" id="parent_id" name="parent_id">
-
-                                <option value="0" selected>ابتدا نوع دسته بندی را مشخص کنید ...</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="form-group col-md-12 mt-3">
-                        <button class="btn btn-outline-primary mt-5" type="submit">ثبت</button>
-                        <a href="{{ route('admin.categories.index') }}" class="btn btn-dark mt-5 mr-3">بازگشت</a>
-                    </div>
+        <div class="col-xl-12 col-md-12 p-4">
+            <div class="card shadow-lg mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between ">
+                    <h5 class="mb-3 mb-md-0">ایجاد دسته بندی</h5>
                 </div>
-            </form>
+                <div>
+                    @include('admin.layouts.partials.errors')
+                    <form action="{{ route('admin.categories.store') }}" method="POST">
+                        @csrf
+                        <div class="form-row p-4">
+                            <div class="form-group col-md-12 mt-3">
+                                <div class="form-group col-md-3">
+                                    <label for="Category_title">نام دسته بندی:</label>
+                                    <input class="form-control" id="Category_title" name="Category_title" type="text"
+                                           value="{{ old('Category_title') }}">
+                                </div>
+                            </div>
+                            <div class="form-group col-md-12 mt-3">
+                                <div class="form-group col-md-3 mt">
+                                    <label for="slug">نام دسته بندی به انگلیسی:</label>
+                                    <input class="form-control" id="slug" name="slug" type="text"
+                                           value="{{ old('slug') }}">
+                                </div>
+                            </div>
+                            <div class="form-group col-md-12 mt-3">
+                                <label for="type" class="pr-3">انتخاب دسته:</label>
+                                <div class="form-group col-md-3" id="select_category">
+                                        <label for="course">دوره های آموزشی</label>
+                                        <input id="course" class="category_type" type="radio" value="1"
+                                               name="cat_type">
+                                        <label for="post" class="mr-2">مقالات</label>
+                                        <input id="post" class="category_type" type="radio" value="2"
+                                               name="cat_type">
+                                </div>
+                            </div>
+                            <div class="form-group col-md-12 mt-3">
+                                <div class="form-group col-md-3">
+                                    <label for="parent_id">نوع دسته</label>
+                                    <select class="form-control category-select" id="parent_id" name="parent_id">
+                                        <option value="0" selected>ابتدا نوع دسته بندی را مشخص کنید ...</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-12 mt-3 mr-3">
+                                <button class="btn btn-outline-success px-4" type="submit">ثبت</button>
+                                <a href="{{ route('admin.categories.index') }}"
+                                   class="btn btn-outline-dark mr-3">بازگشت</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-
 @endsection
 
 @section('script')
@@ -126,8 +127,7 @@
                         response.data.forEach(function (item, index) {
                             let option = `<option value=${item.id}>${item.title}</option>`;
                             $('.category-select').append(option);
-                            });
-
+                        });
 
 
                         // console.log(response.data)
@@ -136,7 +136,6 @@
                 });
             });
         });
-
 
 
     </script>
