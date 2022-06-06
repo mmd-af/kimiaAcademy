de052e2816093598b9eb0a59b70ca07d1c6bf24e
Alimohammad-Hoodi <62663076+Alimohammad-Hoodi@users.noreply.github.com>
2022-06-06 15:04:45 +0430

    Add getParentIdAttribute in ItemModifier

--- app/Models/Item/ItemModifiers.php
+++ app/Models/Item/ItemModifiers.php
@@ -4,5 +4,13 @@ namespace App\Models\Item;
 
 trait ItemModifiers
 {
+    public function getParentIdAttribute($parent_id)
+    {
+        if ($parent_id == 0) {
+            return "فصل";
+        } else {
+            return "درس";
+        }
+    }
 
 }
