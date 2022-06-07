17162aa1235ad634d880311a47959180e3116bc1
mojtaba mohammadi <mojtaba.m2021@gmail.com>
2022-06-07 12:45:49 +0430

    automate fresh seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,18 +5,10 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use function now;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
     public function definition()
     {
         return [
@@ -32,11 +24,6 @@ class UserFactory extends Factory
 
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
     public function unverified()
     {
         return $this->state(function (array $attributes) {
