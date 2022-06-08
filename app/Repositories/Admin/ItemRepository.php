aa4b894850cdf60baf12619fc0b8e00c6caf82d8
mojtaba mohammadi <mojtaba.m2021@gmail.com>
2022-06-08 11:43:31 +0430

    item repository - make getCourse - getAll - getDatatableData methods

--- app/Repositories/Admin/ItemRepository.php
+++ app/Repositories/Admin/ItemRepository.php
@@ -2,7 +2,10 @@
 
 namespace App\Repositories\Admin;
 
+use App\Models\Category\Category;
+use App\Models\Course\Course;
 use App\Models\Item\Item;
+use Yajra\DataTables\Facades\DataTables;
 
 
 class ItemRepository extends BaseRepository
@@ -15,14 +18,54 @@ class ItemRepository extends BaseRepository
     {
         return Item::query()
             ->select([
-                'id',
                 'course_id',
                 'title',
                 'description',
                 'is_free',
                 'parent_id',
-                'sort'
-            ])->get();
+            ])
+            ->orderBy('sort','desc')
+            ->get();
 
+    }
+
+    public function getCourse()
+    {
+        return Course::query()
+            ->select([
+                'id',
+                'title',
+            ])
+            ->where('is_active', 1)
+            ->get();
+    }
+
+
+    public function getDatatableData($request)
+    {
+        if ($request->ajax()) {
+            $data = $this->getAll();
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $edit = route('admin.items.edit', $row->id);
+                    $destroy = route('admin.items.destroy', $row->id);
+                    $c = csrf_field();
+                    $m = method_field('DELETE');
+                    return
+                        "
+                    <div class='d-flex justify-content-center'>
+                    <a href='{$edit}' class='btn btn-outline-info btn-sm mx-2'>ویرایش</a>
+                    <form action='{$destroy}' method='POST'>
+                    $c
+                    $m
+                    <button type='submit' class='btn btn-sm btn-outline-danger'>حذف</button>
+                    </form>
+                    </div>
+                    ";
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
     }
 }
