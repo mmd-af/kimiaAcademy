e245f4aa9a23e112ff3f333afbc2411bb46a380c
mojtaba mohammadi <mojtaba.m2021@gmail.com>
2022-06-08 11:44:10 +0430

    item ajaxController - getDatatableData methods

--- app/Http/Controllers/Admin/Item/ItemAjaxController.php
+++ app/Http/Controllers/Admin/Item/ItemAjaxController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers\Admin\Item;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Admin\ItemRepository;
+use Illuminate\Http\Request;
 
 class ItemAjaxController extends Controller
 {
     protected $itemRepository;
 
-//    public function __construct(ItemRepository $itemRepository)
-//    {
-//        $this->itemRepository = $itemRepository;
-//    }
-//    public function getDatatableData(Request $request)
-//    {
-//        return $this->itemRepository->getDatatableData($request);
-//    }
+    public function __construct(ItemRepository $itemRepository)
+    {
+        $this->itemRepository = $itemRepository;
+    }
+    public function getDatatableData(Request $request)
+    {
+        return $this->itemRepository->getDatatableData($request);
+    }
+
+
 }
