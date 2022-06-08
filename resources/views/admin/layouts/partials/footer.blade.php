07ae29273c289663d62954f57363fbc883f18261
mojtaba mohammadi <mojtaba.m2021@gmail.com>
2022-06-08 13:18:28 +0430

    admin footer copyright

--- resources/views/admin/layouts/partials/footer.blade.php
+++ resources/views/admin/layouts/partials/footer.blade.php
@@ -1,7 +1,35 @@
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-      <div class="copyright text-center my-auto">
-        <span>Copyright &copy; kimyagaracademy.com 1401</span>
-      </div>
-    </div>
-  </footer>
+@include('admin.layouts.partials.copyright')
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+@include('admin.layouts.partials.scroll_top')
+
+<!-- JavaScript-->
+<script src="{{ asset('assets/admin/script/admin.js') }}"></script>
+<script src="{{ asset('assets/admin/libs/ckeditor/ckeditor.js') }}"></script>
+<script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
+<script>
+    // $("#czTag").czMore();
+    var options = {
+        filebrowserImageBrowseUrl: '/filemanager?type=Images',
+        filebrowserImageUploadUrl: '/filemanager/upload?type=Images&_token=',
+        filebrowserBrowseUrl: '/filemanager?type=Files',
+        filebrowserUploadUrl: '/filemanager/upload?type=Files&_token='
+    };
+
+    CKEDITOR.replace('editor', options);
+
+    $('#lfm').filemanager('file');
+</script>
+{{--@include('sweet::alert')--}}
+
+@yield('script')
+
+</body>
+
+</html>
