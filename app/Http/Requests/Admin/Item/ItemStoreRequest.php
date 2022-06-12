3966df268dd760b81e52d923648eee5056420bb0
mojtaba mohammadi <mojtaba.m2021@gmail.com>
2022-06-12 13:20:13 +0430

    item store request

---
+++ app/Http/Requests/Admin/Item/ItemStoreRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Admin\Item;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ItemStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'season' => ['required'],
+            'course' => ['required'],
+            'filepath.*' => ['required'],
+            'is_free.*' => ['required', 'integer'],
+            'editor.*' => ['nullable', 'integer'],
+            'sort.*' => ['required'],
+            'lesson.*' => ['required'],
+//TODO add request persian translate
+        ];
+    }
+}
