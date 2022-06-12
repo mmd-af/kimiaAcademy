5f74c5ca42a1aa8623694d799b820d30879e67e6
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-06-12 15:11:01 +0430

    Fix Faker Slug

--- database/factories/Post/PostFactory.php
+++ database/factories/Post/PostFactory.php
@@ -5,18 +5,25 @@ namespace Database\Factories\Post;
 use App\Models\Post\Post;
 use Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class PostFactory extends Factory
 {
 
     public function definition()
     {
-        $title = $this->faker->realTextBetween(5, 15);
+//        $title = $this->faker->realTextBetween(5, 45);
+        $title = $this->faker->sentence;
+        $slug = Str::slug($title);
+//        $user = User::count() >= 20 ? User::inRandomOrder()->first()->id: User::factory();
+//        $category = Category::count() >= 7 ? Category::inRandomOrder()->first()->id: Category::factory();
+
+
         return [
             'user_id' => 1,
             'title' => $title,
-            'slug' => SlugService::createSlug(Post::class, 'slug', $title),
-            'description' => $this->faker->realTextBetween(500, 1000),
+            'slug' =>  $slug,
+            'description' => $this->faker->realTextBetween(500, 2000),
             'is_active' => 1,
         ];
     }
