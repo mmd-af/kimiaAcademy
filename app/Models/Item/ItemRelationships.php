8a2e7da6fdb09e9df2b320d0413c10b189f2fdb3
mojtaba mohammadi <mojtaba.m2021@gmail.com>
2022-06-13 23:33:50 +0430

    Item Relationship : add course relationship

--- app/Models/Item/ItemRelationships.php
+++ app/Models/Item/ItemRelationships.php
@@ -2,11 +2,17 @@
 
 namespace App\Models\Item;
 
+use App\Models\Course\Course;
+
 trait ItemRelationships
 {
     public function parent()
     {
         return $this->belongsTo(Item::class, 'parent_id', 'id');
+    }
+    public function course()
+    {
+        return $this->belongsTo(Course::class , 'course_id');
     }
 
 }
