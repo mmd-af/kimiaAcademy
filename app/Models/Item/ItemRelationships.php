67a730a403a798f373f5dfb90c5f85fea9bc1c73
Alimohammad-Hoodi <62663076+Alimohammad-Hoodi@users.noreply.github.com>
2022-06-15 15:19:21 +0430

    Add children in ItemRelationships

--- app/Models/Item/ItemRelationships.php
+++ app/Models/Item/ItemRelationships.php
@@ -6,9 +6,14 @@ use App\Models\Course\Course;
 
 trait ItemRelationships
 {
+    public function children()
+    {
+        return $this->hasOne(Item::class, 'id' , 'parent_id');
+    }
+
     public function parent()
     {
-        return $this->belongsTo(Item::class, 'parent_id', 'id');
+        return $this->belongsTo(Item::class, 'parent_id','id');
     }
     public function course()
     {
