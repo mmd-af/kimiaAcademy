8507eeb7320f71a974993fd0d8e5bae952eed14e
Alimohammad-Hoodi <62663076+Alimohammad-Hoodi@users.noreply.github.com>
2022-06-18 21:50:20 +0430

    Add commentable,children,parent,user in CommentRelationships in CommentModel

--- app/Models/Comment/CommentRelationships.php
+++ app/Models/Comment/CommentRelationships.php
@@ -2,9 +2,27 @@
 
 namespace App\Models\Comment;
 
-use App\Models\Category\Category;
+
+use App\Models\User;
 
 trait CommentRelationships
 {
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
+    public function children()
+    {
+        return $this->hasMany(Comment::class,  'parent_id');
+    }
 
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'parent_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
