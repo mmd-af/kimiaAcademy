fbcd433c4033b4af7e90b436ed5e7fd5849aa23c
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-06-18 10:15:59 +0430

    Change DataTable Front in show course

--- resources/views/admin/courses/show.blade.php
+++ resources/views/admin/courses/show.blade.php
@@ -19,9 +19,12 @@
                 </div>
                 <div class="row">
                     <div class="col-6">
+                            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between bg-primary text-light mt-3">
+                                <h6 class="mb-3 mb-md-0">   سر فصل ها</h6>
+                            </div>
                         <div class="card shadow-lg table-responsive p-3">
                             <table
-                                class="table table-bordered table-striped table-secondary text-primary text-center data-table">
+                                class="table table-bordered table-striped text-center data-table">
                                 <thead>
                                 <tr>
                                     <th>ردیف</th>
@@ -35,9 +38,10 @@
                             </table>
                         </div>
                     </div>
-
-
                     <div class="col-6">
+                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between mt-3">
+                            <h6 class="mb-3 mb-md-0">لیست درس ها</h6>
+                        </div>
                         <div class="card shadow-lg table-responsive p-3">
                             <table class="table table-bordered table-striped text-center data-table2">
                                 <thead>
