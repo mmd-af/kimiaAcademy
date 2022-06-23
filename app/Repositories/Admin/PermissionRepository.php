33d012de36c890d70491cf112e3c7a80c1848f02
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-06-23 13:03:32 +0430

    refactor permission repository

--- app/Repositories/Admin/PermissionRepository.php
+++ app/Repositories/Admin/PermissionRepository.php
@@ -3,7 +3,8 @@
 namespace App\Repositories\Admin;
 
 
-use App\Models\Permission\Permission;
+use Spatie\Permission\Models\Permission;
+use Yajra\DataTables\Facades\DataTables;
 
 class PermissionRepository extends BaseRepository
 {
@@ -20,39 +21,36 @@ class PermissionRepository extends BaseRepository
                 'name',
                 'display_name'
             ])
-            ->paginate(10);
+            ->get();
     }
 
-//
-//    public function getDatatableData($request)
-//    {
-//        if ($request->ajax()) {
-//            $data = $this->getAll();
-//            return Datatables::of($data)
-//                ->addIndexColumn()
-//                ->addColumn('action', function ($row) {
-//                    $show = route('admin.courses.show', $row->id);
-//                    $edit = route('admin.courses.edit', $row->id);
-//                    $destroy = route('admin.courses.destroy', $row->id);
-//                    $c = csrf_field();
-//                    $m = method_field('DELETE');
-//                    return
-//                        "
-//                    <div class='d-flex justify-content-center'>
-//                    <a href='{$show}' class='btn btn-outline-primary btn-sm'>سر فصل ها</a>
-//                    <a href='{$edit}' class='btn btn-outline-info btn-sm mx-2'>ویرایش</a>
-//                    <form action='{$destroy}' method='POST' id='myForm'>
-//                    $c
-//                    $m
-//                    <button type='submit' onclick='fireSweetAlert(form); return false' class='btn btn-sm btn-outline-danger'>حذف</button>
-//                    </form>
-//                    </div>
-//                    ";
-//                })
-//                ->rawColumns(['action'])
-//                ->make(true);
-//        }
-//    }
+    public function getDatatableData($request)
+    {
+        if ($request->ajax()) {
+            $data = $this->getAll();
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $edit = route('admin.permissions.edit', $row->id);
+                    $destroy = route('admin.permissions.destroy', $row->id);
+                    $c = csrf_field();
+                    $m = method_field('DELETE');
+                    return
+                        "
+                    <div class='d-flex justify-content-center'>
+                    <a href='{$edit}' class='btn btn-outline-info btn-sm mx-2'>ویرایش</a>
+                    <form action='{$destroy}' method='POST' id='myForm'>
+                    $c
+                    $m
+                    <button type='submit' onclick='fireSweetAlert(form); return false' class='btn btn-sm btn-outline-danger'>حذف</button>
+                    </form>
+                    </div>
+                    ";
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+    }
 
     public function store($request)
     {
@@ -63,34 +61,15 @@ class PermissionRepository extends BaseRepository
         $item->save();
     }
 
-//    public function update($request, $course)
-//    {
-//        DB::beginTransaction();
-//        try {
-//            $course->title = $request->input('title');
-//            $course->slug = $request->input('slug');
-//            $course->description = $request->input('description');
-//            $course->actual_price = $request->input('actual_price');
-//            $course->discount_price = $request->input('discount_price');
-//            $course->is_active = $request->input('is_active');
-//            $course->course_lang = $request->input('course_lang');
-//            $course->course_time = $request->input('course_time');
-//            $course->course_size = $request->input('course_size');
-//            $course->course_kind = $request->input('course_kind');
-//            $course->save();
-//            $course->categories()->sync($request->input('category_id'));
-//            $course->videos()->update(['url' => $request->input('url')]);
-//            $course->images()->update(['url' => $request->input('image_url')]);
-//            DB::commit();
-//            return $course;
-//        } catch (\Exception $error) {
-//            DB::rollback();
-//            return $error;
-//        }
-//    }
-//
-//    public function destroy($course)
-//    {
-//        $course->delete();
-//    }
+    public function update($request, $permission)
+    {
+        $permission->name = $request->input('name');
+        $permission->display_name = $request->input('display_name');
+        $permission->save();
+    }
+
+    public function destroy($permission)
+    {
+        $permission->delete();
+    }
 }
