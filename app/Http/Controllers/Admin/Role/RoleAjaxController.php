0cee97870188f732651c9078ddc6ba17817e9074
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-06-23 13:37:20 +0430

    Refactor roles controller

--- app/Http/Controllers/Admin/Role/RoleAjaxController.php
+++ app/Http/Controllers/Admin/Role/RoleAjaxController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers\Admin\Role;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Admin\RoleRepository;
+use Illuminate\Http\Request;
 
 class RoleAjaxController extends Controller
 {
+    protected $roleRepository;
 
+    public function __construct(RoleRepository $roleRepository)
+    {
+        $this->roleRepository = $roleRepository;
+    }
+    public function getDatatableData(Request $request)
+    {
+        return $this->roleRepository->getDatatableData($request);
+    }
 }
--- app/Http/Controllers/Admin/Role/RoleController.php
+++ app/Http/Controllers/Admin/Role/RoleController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\Admin\Role;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Role\RoleStoreRequest;
+use App\Http\Requests\Admin\Role\RoleUpdateRequest;
 use App\Repositories\Admin\RoleRepository;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
     protected $roleRepository;
 
-    public function __construct(RoleRepository $RoleRepository)
+    public function __construct(RoleRepository $roleRepository)
     {
-        $this->roleRepository = $RoleRepository;
+        $this->roleRepository = $roleRepository;
     }
 
     public function index()
@@ -20,42 +23,35 @@ class RoleController extends Controller
         return view('admin.roles.index', compact('roles'));
     }
 
-//    public function create()
-//    {
-//        $categories = $this->roleRepository->getCategory();
-//        return view('admin.roles.create', compact('categories'));
-//    }
-//
-//    public function store(RoleStoreRequest $request)
-//    {
-//        $this->roleRepository->store($request);
-//        alert()->success("با تشکر", 'دوره ی مورد نظر با موفقیت ثبت شد');
-//        return redirect()->route('admin.roles.index');
-//    }
-//
-//    public function show(Role $role, Item $item)
-//    {
-//        return view('admin.roles.show', compact('role', 'item'));
-//    }
-//
-//    public function edit(Role $role)
-//    {
-//        $roleCategory = $role->categories->first();
-//        $roleVideo = $role->videos();
-//        $categories = $this->roleRepository->getCategory();
-//        return view('admin.roles.edit', compact('role', 'categories', 'roleCategory', 'roleVideo'));
-//    }
-//
-//    public function update(PostUpdateRequest $request, Role $role)
-//    {
-//        $this->roleRepository->update($request, $role);
-//        alert()->success("با تشکر", 'دوره ی مورد نظر با موفقیت ویرایش شد');
-//        return redirect()->route('admin.roles.index');
-//    }
-//
-//    public function destroy(Role $role)
-//    {
-//        $this->roleRepository->destroy($role);
-//        return redirect()->route('admin.roles.index');
-//    }
+    public function create()
+    {
+        $permissions = $this->roleRepository->getPermissions();
+        return view('admin.roles.create', compact('permissions'));
+    }
+
+    public function store(RoleStoreRequest $request)
+    {
+        $this->roleRepository->store($request);
+        alert()->success("با تشکر", 'نقش مورد نظر با موفقیت ثبت شد');
+        return redirect()->route('admin.roles.index');
+    }
+
+    public function edit(Role $role)
+    {
+        $permissions = $this->roleRepository->getPermissions();
+        return view('admin.roles.edit', compact('role', 'permissions'));
+    }
+
+    public function update(RoleUpdateRequest $request, Role $role)
+    {
+        $this->roleRepository->update($request, $role);
+        alert()->success("با تشکر", 'نقش مورد نظر با موفقیت ویرایش شد');
+        return redirect()->route('admin.roles.index');
+    }
+
+    public function destroy(Role $role)
+    {
+        $this->roleRepository->destroy($role);
+        return redirect()->route('admin.roles.index');
+    }
 }
