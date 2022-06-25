4f175a7b9178240bf81cb66ba6c1713427e0b54f
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-06-25 17:49:02 +0430

    Add Permission to Sidebar

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -1,18 +1,26 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion pr-0" id="accordionSidebar">
+<ul class="navbar-nav adminSliderColor sidebar sidebar-dark accordion pr-0" id="accordionSidebar">
+{{--TODO after the Deploy Remove this comment files--}}
+{{--
+categoryControl
+courseControl
+postControl
+educationControl
+commentControl
+messageControl
 
+--}}
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('admin.dashboard')}}">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('site.home.index')}}">
         {{--        <div class="sidebar-brand-icon rotate-n-15">--}}
         {{--            <i class="fas fa-laugh-wink"></i>--}}
         {{--        </div>--}}
         <div class="sidebar-brand-text">kimyagaracademy.com</div>
     </a>
-<!-- Divider -->
+    <!-- Divider -->
     <hr class="sidebar-divider my-0">
-
     <!-- Nav Item - Dashboard -->
     <li class="nav-item ">
-        <a class="nav-link" href="{{ route('admin.dashboard') }}">
+        <a class="nav-link" href="{{ route('admin.dashboard.index') }}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span> داشبورد </span></a>
     </li>
@@ -24,45 +32,55 @@
     <div class="sidebar-heading">
         مدیریت
     </div>
-
+@can('categoryControl')
     <!-- Nav Item - categories -->
-    <li class="nav-item">
-        <a class="nav-link" href="{{route('admin.categories.index')}}">
-            <i class="fas fa-window-restore"></i>
-            <span>دسته بندی ها </span>
-        </a>
-    </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('admin.categories.index')}}">
+                <i class="fas fa-window-restore"></i>
+                <span>دسته بندی ها </span>
+            </a>
+        </li>
+    @endcan
+    @can('courseControl')
     <li class="nav-item">
         <a class="nav-link" href="{{route('admin.courses.index')}}">
             <i class="fas fa-film"></i>
             <span>دوره های آموزشی</span>
         </a>
     </li>
+    @endcan
+    @can('postControl')
     <li class="nav-item">
         <a class="nav-link" href="{{route('admin.posts.index')}}">
             <i class="fas fa-file"></i>
             <span> مقالات </span>
         </a>
     </li>
+    @endcan
+    @can('educationControl')
     <li class="nav-item">
         <a class="nav-link" href="{{route('admin.educationalvideos.index')}}">
             <i class="fas fa-video"></i>
             <span> ویدئو های آموزشی </span>
         </a>
     </li>
+    @endcan
+    @can('commentControl')
     <li class="nav-item">
         <a class="nav-link" href="{{route('admin.comments.index')}}">
             <i class="fas fa-comment"></i>
             <span> نظرات </span>
         </a>
     </li>
+    @endcan
+    @can('messageControl')
     <li class="nav-item">
         <a class="nav-link" href="{{route('admin.messages.index')}}">
             <i class="fas fa-object-ungroup"></i>
             <span> پیام ها </span>
         </a>
     </li>
-
+    @endcan
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
 
