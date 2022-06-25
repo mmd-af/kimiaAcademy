667d9e4a8f21fc3a7d5a5ef4f062b03fb4e99c06
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-06-25 17:50:45 +0430

    Add Permission to Route

--- routes/web/admin/PostRoute.php
+++ routes/web/admin/PostRoute.php
@@ -2,25 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['web', 'auth'], 'namespace' => 'App\Http\Controllers\Admin\Post'], function () {
+Route::group(['middleware' => ['web', 'auth', 'permission:postControl'], 'namespace' => 'App\Http\Controllers\Admin\Post'], function () {
     Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
         Route::group(['prefix' => 'posts', 'as' => 'posts.'], function () {
-
             Route::get('/', [
                 'as' => 'index',
                 'uses' => 'PostController@index'
             ]);
-
             Route::get('/create', [
                 'as' => 'create',
                 'uses' => 'PostController@create'
             ]);
-
             Route::post('/store', [
                 'as' => 'store',
                 'uses' => 'PostController@store'
             ]);
-
             Route::get('/{post}/edit', [
                 'as' => 'edit',
                 'uses' => 'PostController@edit'
@@ -29,15 +25,11 @@ Route::group(['middleware' => ['web', 'auth'], 'namespace' => 'App\Http\Controll
                 'as' => 'update',
                 'uses' => 'PostController@update'
             ]);
-
             Route::delete('{post}/destroy', [
                 'as' => 'destroy',
                 'uses' => 'PostController@destroy'
             ]);
-
-
         });
-
         Route::group(['middleware' => ['is.ajax'], 'prefix' => 'posts-ajax', 'as' => 'posts.ajax.'], function () {
             Route::get('/getDatatableData', [
                 'as' => 'getDatatableData',
@@ -46,5 +38,3 @@ Route::group(['middleware' => ['web', 'auth'], 'namespace' => 'App\Http\Controll
         });
     });
 });
-
-
