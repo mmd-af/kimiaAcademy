f64032ba8354e474544fd4d751bad9e7770c84df
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-06-25 17:45:46 +0430

    Add SweetAlert to Login

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,14 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $email = Auth::user()->email;
-        alert()->success("کاربر ".$email, 'خوش آمدید');
-        return view('admin.dashboard.dashboard');
+        return view('admin.dashboard.index');
     }
 }
