6c45d9c763c80f3ce94ece390d9e70f75c9628eb
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-06-29 23:21:27 +0430

    Add Reza To Home Page

--- resources/views/site/layouts/partials/header.blade.php
+++ resources/views/site/layouts/partials/header.blade.php
@@ -1,21 +1,17 @@
-
-<div class="container-fluid bg-header py-4">
+<div class="container-fluid bg-header pt-4">
     <div class="container">
         <div class="row gx-5">
             <div class="col-md-5">
-                <figure class="figure">
-                    <img src="holder.js/400x400" class="figure-img img-fluid rounded"
-                         alt="A generic square placeholder image with rounded corners in a figure.">
-                </figure>
+                <img src="{{asset('assets/site/images/rezamoafpourian.png')}}" class="img-fluid"
+                     alt="رضا معاف پوریان">
             </div>
             <div class="col-md-7 p-4">
                 <h1 class="text-light ">به آکادمی کیمیاگر خوش آمدید</h1>
-                <p class="text-light text-justify py-4 ">
+                <p class="text-light text-justify py-4">
                     <span>
                           در این مجموعه سعی بر این است که با تلفیقی از طب سنتی و داروسازی مدرن، بتوانیم به تندرستی بدن و
                     بهبودی بیماری ها کمک کنیم.
                     </span>
-
                 </p>
             </div>
         </div>
