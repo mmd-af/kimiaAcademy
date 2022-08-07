395b5dfb3f45c104738714230417e0e01c90b8c2
Ali <alimohammad.hoodi@gmail.com>
2022-08-07 21:41:46 +0430

    Refactor dropdown items in user & admin dashboard

--- resources/views/admin/layouts/partials/topbar.blade.php
+++ resources/views/admin/layouts/partials/topbar.blade.php
@@ -21,15 +21,6 @@
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in text-right"
                  aria-labelledby="userDropdown">
-{{--                <a class="dropdown-item" href="{{route('user.users.index')}}">--}}
-{{--                    <i class="fas fa-user fa-sm fa-fw ml-2 text-gray-400"></i>--}}
-{{--                    پروفایل--}}
-{{--                </a>--}}
-                {{--                <a class="dropdown-item" href="#">--}}
-                {{--                    <i class="fas fa-cogs fa-sm fa-fw ml-2 text-gray-400"></i>--}}
-                {{--                    تنظیمات--}}
-                {{--                </a>--}}
-                <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw ml-2 text-gray-400"></i>
                     خروج
