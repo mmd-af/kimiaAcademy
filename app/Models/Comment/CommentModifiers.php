fb8773054c592e02d3b5f225c26e81956d36ef17
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-08-22 10:36:47 +0430

    Add the IMPORTANT TODO in the CommentModifires

--- app/Models/Comment/CommentModifiers.php
+++ app/Models/Comment/CommentModifiers.php
@@ -8,8 +8,8 @@ trait CommentModifiers
 {
     public function getUserIdAttribute($user_id)
     {
+        // TODO be khatere in toye view/ site/ course/ show query ezafi mikhore
         $user = User::findOrFail($user_id);
         return $user->firstname . ' ' . $user->lastname;
     }
-
 }
